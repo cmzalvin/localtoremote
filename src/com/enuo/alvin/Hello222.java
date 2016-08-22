f646a9853d818a5c21b67ba9156d9970473f12eb
cmzalvin <350906358@qq.com>
2016-08-22 17:43:39 +0800

    第一次提交

---
+++ src/com/enuo/alvin/Hello222.java
@@ -0,0 +1,12 @@
+package com.enuo.alvin;
+
+/**
+ * Created by e诺
+ * on 16/8/22
+ * Time 下午5:01
+ */
+public class Hello222 {
+    public static void main(String[] args) {
+        System.out.println("hello22222");
+    }
+}
